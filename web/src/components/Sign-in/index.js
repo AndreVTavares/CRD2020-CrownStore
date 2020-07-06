f88009741bdf59e0385028adc2b6095d12fab491
AndreVTavares <andretavares3@gmail.com>
2020-07-06 14:22:57 -0300

    Sign Up component creating an user and store it on firebase database

--- web/src/components/Sign-in/index.js
+++ web/src/components/Sign-in/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 import './styles.scss';
 
@@ -12,9 +12,6 @@ const SignIn = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     
-    useEffect(() => {
-
-    }, []);
 
     const handleSubmitForm = (event) => {
         event.preventDefault();
--- web/src/pages/SignInUp/index.js
+++ web/src/pages/SignInUp/index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './styles.scss';
 
 import SignIn from '../../components/Sign-in';
+import SignUp from '../../components/Sign-up';
 
 const SignInUpPage = () => {
     return (
-        <div className='sign-in-up'>
+        <div className='sign-in-and-sign-up'>
             <SignIn />
+            <SignUp />
         </div>
     )
 }
