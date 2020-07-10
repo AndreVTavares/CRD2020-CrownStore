ed633e9af4044a478a961f972f939011837b9336
AndreVTavares <andretavares3@gmail.com>
2020-07-10 09:16:56 -0300

    creating user selectors and changing mapstatetoprops to use create structured selectors

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 
@@ -10,6 +11,7 @@ import SignInUpPage from './pages/SignInUp';
 import Header from './components/Header';
 
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 import { auth, createUserProfileDocument } from './firebase/firebase'
 
@@ -68,8 +70,8 @@ class App extends React.Component {
   } 
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase';
 import CartIcon from '../Cart-icon';
 import CartDropdown from '../Cart-dropdown';
+
+import { selectHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 import './styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg'
@@ -43,10 +47,10 @@ const Header = ({ currentUser, hidden }) => {
 };
 
 
-const mapStateToProps = (state) => ({
+const mapStateToProps =  createStructuredSelector({
 
-  currentUser: state.user.currentUser,
-  hidden: state.cart.hidden
+  currentUser: selectCurrentUser,
+  hidden: selectHidden
 
 })
 
