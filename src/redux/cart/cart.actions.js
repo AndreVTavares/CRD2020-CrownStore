13e0c1080a073ac0924f8aa4ed07efea801e4628
AndreVTavares <andretavares3@gmail.com>
2020-09-16 11:34:21 -0300

    clear cart when user sign out

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,20 +1,24 @@
-import { CartActionTypes } from './cart.types';
+import cartActionTypes from './cart.types';
 
 export const toggleCartHidden = () => ({
-    type: CartActionTypes.TOGGLE_CART_HIDDEN
+    type: cartActionTypes.TOGGLE_CART_HIDDEN
 })
 
 export const addItem = item => ({
-    type: CartActionTypes.ADD_ITEM,
+    type: cartActionTypes.ADD_ITEM,
     payload: item,
 })
 
 export const removeItem = item => ({
-    type: CartActionTypes.REMOVE_ITEM,
+    type: cartActionTypes.REMOVE_ITEM,
     payload: item,
 })
 
 export const clearItemFromCart = item => ({
-    type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+    type: cartActionTypes.CLEAR_ITEM_FROM_CART,
     payload: item,
+})
+
+export const clearCart = () => ({
+    type: cartActionTypes.CLEAR_CART
 })
